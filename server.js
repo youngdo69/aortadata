a381b03ed48ddd6fae04c9cae9c59e7874954a6c
youngdo69 <67508881+youngdo69@users.noreply.github.com>
2023-02-28 13:23:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
+const s3 = new AWS.S3();
 
 const cors = require('cors');
 const fs = require('fs');
